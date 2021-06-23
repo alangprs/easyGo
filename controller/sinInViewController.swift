303eec7f50fff6011f2dee2da0e591ecdd7dbeca
will <alangprs1@gmail.com>
2021-06-23 10:16:59 +0800

    第三頁基本畫面完成（未完成底層view)

---
+++ controller/sinInViewController.swift
@@ -0,0 +1,29 @@
+//
+//  sinInViewController.swift
+//  easyGo
+//
+//  Created by 翁燮羽 on 2021/6/22.
+//
+
+import UIKit
+
+class sinInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
